7e13c35cf5f6ee678f8644ffcae917dde1ea370a
Ceddy Muhoza <ceddymuhoza@gmail.com>
2021-02-03 03:05:15 -0500

    export UserResolverWeb

--- backend/src/resolvers.ts
+++ backend/src/resolvers.ts
@@ -3,6 +3,7 @@ import {NonEmptyArray} from "type-graphql/dist/interfaces/NonEmptyArray";
 
 import UserResolver from './user/User.resolver';
 import UserResolverAdmin from './user/User.resolver.admin';
+import UserResolverWeb from './user/User.resolver.web';
 import WalletResolver from './wallet/Wallet.resolver';
 import MarketDataResolver from './marketdata/Marketdata.resolver';
 import TradeResolver from './trade/Trade.resolver';
@@ -14,4 +15,4 @@ import PortfolioResolver from './portfolio/Portfolio.resolver';
 
 
 
-export const Resolvers: NonEmptyArray<Function> | any = () => [UserResolver, UserResolverAdmin, WalletResolver, MarketDataResolver, TradeResolver, TransactionResolver, TransactionAdminResolver, PaymentMethodResolver, PortfolioResolver]
+export const Resolvers: NonEmptyArray<Function> | any = () => [UserResolver, UserResolverAdmin,UserResolverWeb, WalletResolver, MarketDataResolver, TradeResolver, TransactionResolver, TransactionAdminResolver, PaymentMethodResolver, PortfolioResolver]
--- backend/src/user/index.ts
+++ backend/src/user/index.ts
@@ -1,2 +1,3 @@
 export * from './User.model';
 export * from './User.resolver';
+export * from './User.resolver.web';
